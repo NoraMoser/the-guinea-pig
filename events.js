7e75a2056ae0e252adc5288890ced426e55489fd
Nora Moser <noramoser@Noras-MBP.hsd1.tn.comcast.net>
2017-07-25 20:38:40 -0500

    halfway there

---
+++ events.js
@@ -0,0 +1,56 @@
+console.log("hi");
+
+let output = "You have clicked ";
+let mouseOver = "You have moved your mouse over ";
+let outputTarget = document.getElementById("output-target");
+let inputTarget = document.getElementById("keypress-input");
+let colorButton = document.getElementById("add-color");
+let guineaPig = document.getElementById("guinea-pig");
+
+document.getElementById("intro").addEventListener("click", function(){
+    document.getElementById("output-target").innerHTML = output + "The introduction."
+   
+});
+
+document.getElementById("body").addEventListener("click", function(){
+    document.getElementById("output-target").innerHTML = output + "The body."
+  
+});
+
+document.getElementById("section1").addEventListener("click", function(){
+    document.getElementById("output-target").innerHTML = output + "Section one."
+  
+});
+
+document.getElementById("section2").addEventListener("click", function(){
+    document.getElementById("output-target").innerHTML = output + "Section two."
+  
+});
+document.getElementById("section3").addEventListener("click", function(){
+    document.getElementById("output-target").innerHTML = output + "Section three."
+  
+});
+
+document.getElementById("conclusion").addEventListener("click", function(){
+    document.getElementById("output-target").innerHTML = output + "The conclusion."
+  
+});
+
+document.getElementById("page-title").addEventListener("mouseover", function(){
+    document.getElementById("output-target").innerHTML = mouseOver + "The header."
+  
+});
+
+document.getElementById("page-title").addEventListener("mouseout", function(){
+    document.getElementById("output-target").innerHTML = "You left me!"
+  
+});
+
+inputTarget.addEventListener("keyup", function(event){
+	// console.log("event", event);
+	outputTarget.innerHTML = event.target.value;
+});
+
+colorButton.addEventListener("click", function() {   
+    guineaPig.classList.toggle("blue");
+});
